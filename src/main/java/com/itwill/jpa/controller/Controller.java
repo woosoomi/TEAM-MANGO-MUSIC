d489aa8f309b4f65d02c635e40d3024f8082ffdb
WooHanYeong <woocoding96@gmail.com>
2023-10-16 19:39:43 +0900

    11

--- src/main/java/com/itwill/jpa/controller/Controller.java
+++ src/main/java/com/itwill/jpa/controller/Controller.java
@@ -5,12 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 	
-	@GetMapping("/userfavoritegrid")
-	public String userfavoritegrid() {
-		String forwardPath ="userfavoritegrid";
-		return forwardPath;
-		
-	}
 	@GetMapping("/index")
 	public String index() {
 		String forwardPath ="index";
