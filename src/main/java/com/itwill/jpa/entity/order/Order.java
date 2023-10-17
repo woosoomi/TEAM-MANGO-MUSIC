cacaa170e08a7a437cd963a6070a5d0bc8d3291c
차경진 <ckrudwls7@naver.com>
2023-10-17 18:03:25 +0900

    ...

--- src/main/java/com/itwill/jpa/entity/order/Order.java
+++ src/main/java/com/itwill/jpa/entity/order/Order.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,6 +29,8 @@ public class Order {
 	
 	private String status;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private String userid; //FK
 
 	private List<OrderItem> orderitems;
