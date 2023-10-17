5d3c4e1826b0a3c5fc13624eb52df98d92634c3e
유채린 <cofls000@naver.com>
2023-10-17 10:36:28 +0900

    User

--- src/test/java/com/itwill/UserRepositoryTest.java
+++ src/test/java/com/itwill/UserRepositoryTest.java
@@ -17,14 +17,20 @@ class UserRepositoryTest {
 	@Test
 
 	void testSave() {
+
 		User user1=User.builder()
-					.userId("guard21")
-					.password("2121")
-					.name("김경호21")
-					.email("guard21@gmail.com")
+					.user_id("guard21")
+					.user_pw("2121")
+					.user_name("김경호21")
+					.user_phone("010-1234-5678")
+					.user_address("guard21@gmail.com")
+					.user_email("guard21@gmail.com")
+					.user_jumin("20231017-2***")
+					.user_gender("여성")
 					.build();
-		User savedUser1 = userRepository.save(user1);
-		System.out.println(savedUser1);
+		//User savedUser1 = userRepository.save(user1);
+		//System.out.println(savedUser1);
+
 		
 	}
 	
