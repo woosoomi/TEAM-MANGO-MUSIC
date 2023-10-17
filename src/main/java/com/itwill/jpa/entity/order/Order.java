9c3edb283037eaa5ab5e2477d0de20ac539bea17
차경진 <ckrudwls7@naver.com>
2023-10-17 16:13:13 +0900

    .

--- src/main/java/com/itwill/jpa/entity/order/Order.java
+++ src/main/java/com/itwill/jpa/entity/order/Order.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.itwill.jpa.dto.order.OrderDto;
 import com.itwill.jpa.dto.order.OrderItemDto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,6 +28,7 @@ import lombok.NoArgsConstructor;
 public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "order_no")
 	private Long orderid; //PK
 
 	private String ordername;
