db12922bf3c0a15773a703a95e5669783780b27f
rhqkf7496 <rhqkf7496@naver.com>
2023-10-17 17:33:52 +0900

    ..

--- src/main/java/com/itwill/jpa/dao/user/UserServiceImpl.java
+++ src/main/java/com/itwill/jpa/dao/user/UserServiceImpl.java
@@ -10,18 +10,18 @@ public class UserServiceImpl implements UserService{
 	
 	// 회원가입
 	@Override
-	public int create(UserDto user_id) throws Exception {
-		if (userDao.existedUser(user_id.getUser_id())) {
+	public int create(UserDto userid) throws Exception {
+		if (userDao.existedUser(userid.getUserid())) {
 			//중복인 경우, exception 처리 만들어야함.
-		}return userDao.create(user_id);
+		}return userDao.create(userid);
 	}
 
 	
 	// 로그인
 	@Override
-	public int login(String user_id, String password) throws Exception {
-		UserDto user = userDao.findUser(user_id);
-		UserDto fUser = new UserDto(user_id, password, "", "", "", "", "", "");
+	public int login(String userid, String password) throws Exception {
+		UserDto user = userDao.findUser(userid);
+		UserDto fUser = new UserDto(userid, password, "", "", "", "", "", "");
 		if (user==null) {
 			
 		}if (user.equals(password)) {
@@ -33,19 +33,19 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public UserDto finduser_id(String user_id) throws Exception {
+	public UserDto finduser_id(String userid) throws Exception {
 		
 		return null;
 	}
 
 	@Override
-	public int update(UserDto user_id) throws Exception {
+	public int update(UserDto userid) throws Exception {
 		
 		return 0;
 	}
 
 	@Override
-	public int remove(String user_id) throws Exception {
+	public int remove(String userid) throws Exception {
 		
 		return 0;
 	}
@@ -57,7 +57,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public boolean isDuplicateId(String user_id) throws Exception {
+	public boolean isDuplicateId(String userid) throws Exception {
 		
 		return false;
 	}
--- src/main/java/com/itwill/jpa/dto/user/UserDto.java
+++ src/main/java/com/itwill/jpa/dto/user/UserDto.java
@@ -1,123 +1,31 @@
 package com.itwill.jpa.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /*
  *  사용자관리를 위하여 필요한 도메인클래스(VO,DTO)
  *  USERINFO 테이블의 각컬럼에해당하는 멤버를 가지고있다
  */
 
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
 	
 	private String userid;			// 회원 아이디
 	private String userpw;			// 회원 비밀번호
 	private String username;		// 회원 이름
-	private String useraddress;	// 회원 주소
+	private String useraddress;		// 회원 주소
 	private String useremail;		// 회원 이메일
 	private String userjumin;		// 회원 주민번호
 	private String userphone;		// 회원 전화번호
 	private String usergender;		// 회원 성별
-	
-	public UserDto() {
-		
-	}
-
-	public UserDto(String userid, String userpw, String username, String useraddress, String useremail,
-			String userjumin, String userphone, String usergender) {
-		super();
-		this.userid = userid;
-		this.userpw = userpw;
-		this.username = username;
-		this.useraddress = useraddress;
-		this.useremail = useremail;
-		this.userjumin = userjumin;
-		this.userphone = userphone;
-		this.usergender = usergender;
-	}
-	
-	public String getUserid() {
-		return userid;
-	}
-
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-
-	public String getUserpw() {
-		return userpw;
-	}
-
-
-	public void setUserpw(String userpw) {
-		this.userpw = userpw;
-	}
-
-
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getUseraddress() {
-		return useraddress;
-	}
-
-
-	public void setUseraddress(String useraddress) {
-		this.useraddress = useraddress;
-	}
-
-
-	public String getUseremail() {
-		return useremail;
-	}
-
-
-	public void setUseremail(String useremail) {
-		this.useremail = useremail;
-	}
-
-
-	public String getUserjumin() {
-		return userjumin;
-	}
-
-
-	public void setUserjumin(String userjumin) {
-		this.userjumin = userjumin;
-	}
-
-
-	public String getUserphone() {
-		return userphone;
-	}
-
-
-	public void setUserphone(String userphone) {
-		this.userphone = userphone;
-	}
-
-
-	public String getUsergender() {
-		return usergender;
-	}
-
-
-	public void setUsergender(String usergender) {
-		this.usergender = usergender;
-	}
-
-	@Override
-	public String toString() {
-		return "UserDto [userid=" + userid + ", userpw=" + userpw + ", username=" + username + ", useraddress="
-				+ useraddress + ", useremail=" + useremail + ", userjumin=" + userjumin + ", userphone=" + userphone
-				+ ", usergender=" + usergender + "]";
-	}
 	
 }
 
