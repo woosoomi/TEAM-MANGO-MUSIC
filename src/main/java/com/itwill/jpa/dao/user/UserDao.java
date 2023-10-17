5feb511daa4369ba4d2cb440959a2a286ffb3f8e
rhqkf7496 <rhqkf7496@naver.com>
2023-10-17 18:59:38 +0900

    ..

--- src/main/java/com/itwill/jpa/dao/user/UserDao.java
+++ src/main/java/com/itwill/jpa/dao/user/UserDao.java
@@ -2,26 +2,20 @@ package com.itwill.jpa.dao.user;
 
 import java.util.List;
 
-import com.itwill.jpa.dto.user.UserDto;
+import com.itwill.jpa.entity.user.User;
 
 public interface UserDao {
 
-	// 사용자관리테이블에 새로운사용자생성
-	int create(UserDto user) throws Exception;
+	User createUser(User user);
 
-	// 기존의 사용자정보를 수정
-	int update(UserDto user) throws Exception;
+    User updateUser(User user);
 
-	// 사용자아이디에해당하는 사용자를 삭제
-	int remove(String user_id) throws Exception;
+    void removeUser(String id);
 
-	// 사용자아이디에해당하는 정보를 데이타베이스에서 찾아서 User 도메인클래스에 저장하여 반환
-	UserDto findUser(String user_id) throws Exception;
-	
-	// 모든사용자 정보를 데이타베이스에서 찾아서 List<User> 콜렉션 에 저장하여 반환
-	List<UserDto> findUserList() throws Exception;
-	
-	// 인자로 전달되는 아이디를 가지는 사용자가 존재하는지의 여부를 판별
-	boolean existedUser(String user_id) throws Exception;
+    User findUser(String id);
+
+    List<User> findUserList();
+    
+    boolean existedUser(String userid);
 
 }
--- src/main/java/com/itwill/jpa/service/user/UserServiceImpl.java
+++ src/main/java/com/itwill/jpa/service/user/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.itwill.jpa.dao.user.UserDao;
 import com.itwill.jpa.dto.user.UserDto;
+import com.itwill.jpa.entity.user.User;
 
 public class UserServiceImpl implements UserService{
 	private UserDao userDao;
@@ -12,48 +13,41 @@ public class UserServiceImpl implements UserService{
 	
 	// 회원가입
 	@Override
-	public int create(UserDto userid) throws Exception {
+	public User createUser(User userid) throws Exception {
 		if (userDao.existedUser(userid.getUserid())) {
 			//중복인 경우, exception 처리 만들어야함.
-		}return userDao.create(userid);
+		}return userDao.createUser(userid);
 	}
 
 	
 	// 로그인
 	@Override
 	public int login(String userid, String password) throws Exception {
-		UserDto user = userDao.findUser(userid);
-		UserDto fUser = new UserDto(userid, password, "", "", "", "", "", "");
-		if (user==null) {
-			
-		}if (user.equals(password)) {
-			
-		}
 		
 		return 0;
 		
 	}
 
 	@Override
-	public UserDto finduser_id(String userid) throws Exception {
+	public User findUser(String userid) throws Exception {
 		
 		return null;
 	}
 
 	@Override
-	public int update(UserDto userid) throws Exception {
+	public int updateUser(User userid) throws Exception {
 		
 		return 0;
 	}
 
 	@Override
-	public int remove(String userid) throws Exception {
+	public int removeUser(String userid) throws Exception {
 		
 		return 0;
 	}
 
 	@Override
-	public List<UserDto> finduser_idList() throws Exception {
+	public List<User> findUserList() throws Exception {
 		
 		return null;
 	}
