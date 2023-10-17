58ed24414c21e943c34c4aaf67cc6a87946a67af
woosoomi <tnaldn01@gmail.com>
2023-10-17 18:22:43 +0900

    product entity - 싱글테이블 매핑? maybe... Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team1-mango.git

--- src/main/java/com/itwill/jpa/entity/product/Product.java
+++ src/main/java/com/itwill/jpa/entity/product/Product.java
@@ -1,5 +1,6 @@
 package com.itwill.jpa.entity.product;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale.Category;
@@ -17,18 +18,61 @@ import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@DiscriminatorColumn(name = "DTYPE")
+@Getter
+@Setter
 public class Product {
-	 @Id @GeneratedValue
-	 @Column(name = "productId")
-	 private Long id;
-	 
-	 @Column(nullable = false)
-	 private String name;
-	 @Column(nullable = false)
-	 private int price;
-	 @ManyToMany(mappedBy = "products")
-	 private List<Category> categories = new ArrayList<Category>();
+	@Id
+	@GeneratedValue
+	@Column(name = "productId")
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private int price;
+	@ManyToMany(mappedBy = "products")
+	private List<Category> categories = new ArrayList<Category>();
+
+	/** music **/
+	@Entity
+	public class Music extends Product {
+		private String movie;
+		private String artist;
+		private String content;
+		private String reply;
+		private String star;
+		private Date date;
+		private Long readCount;
 	}
 
+	/** goods **/
+	@Entity
+	public class Goods extends Product {
+		// private String content;
+		// private String reply;
+		// private Date date;
+		// private String star;
+		private int stock;
+	}
+
+	/** ticket **/
+	public class Ticket extends Product {
+		private String address;
+		// private String content;
+		// private String reply;
+		// private Date date;
+
+		// private Long readCount;
+		// private int stock;
+	}
+
+	/** membership **/
+	@Entity
+	public class Membership extends Product {
+		private Date startPeriod;
+		private int periodOfUse;
+	}
+	// private User userId;
+
+}
