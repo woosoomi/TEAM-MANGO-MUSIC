ae4f73017e0f3f824aeee3391eda02133d31dc84
유채린 <cofls000@naver.com>
2023-10-17 14:22:15 +0900

    ..

--- src/main/java/com/itwill/jpa/controller/Controller.java
+++ src/main/java/com/itwill/jpa/controller/Controller.java
@@ -2,7 +2,7 @@ package com.itwill.jpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.stereotype.Controller
+
 public class Controller {
 	
 	@GetMapping("/index")
