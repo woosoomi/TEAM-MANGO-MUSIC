f3ebc31282f404aadf6c858a193db65d73828094
유채린 <cofls000@naver.com>
2023-10-17 16:39:15 +0900

    vote

--- src/main/java/com/itwill/jpa/dto/vote/Vote.java
+++ src/main/java/com/itwill/jpa/dto/vote/Vote.java
@@ -1,14 +1,18 @@
 package com.itwill.jpa.dto.vote;
 
-import com.itwill.jpa.dto.user.User;
+import java.sql.Date;
 
 public class Vote {
 	
 	
-	// 용어 수정 예정 임의로 작성
-	private User user;	// 	회원
-	private int replay;	//  음악플레이수
-	private int reply;	//  댓글 수
-	private int likes;	//  좋아요 수
+	
+	// 용어 수정 예정--> 이름 임의로 작성하였으며 추후 통일 예정
+	private String userId;	 // 회원 아이디
+	
+	private int musicReplay; //  음악 플레이 횟수
+	private int replyCount;	 //  음악 댓글 수
+	private int likes;		 //  음악 좋아요 수
+	private int vote;		 //  투표 수
+	private Date date; 		 //  투표 날짜
 	
 }
---
+++ src/main/java/com/itwill/jpa/repository/vote/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.itwill.jpa.repository.vote;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itwill.jpa.entity.vote.Vote;
+
+
+
+public interface VoteRepository extends JpaRepository<Vote, Long>{
+	
+	
+}
