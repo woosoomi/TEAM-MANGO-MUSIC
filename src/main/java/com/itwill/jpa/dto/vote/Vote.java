68100df0abb322ade838222a0c0bc2b18f75c2aa
유채린 <cofls000@naver.com>
2023-10-17 16:06:36 +0900

    vote 추가

---
+++ src/main/java/com/itwill/jpa/dto/vote/Vote.java
@@ -0,0 +1,14 @@
+package com.itwill.jpa.dto.vote;
+
+import com.itwill.jpa.dto.user.User;
+
+public class Vote {
+	
+	
+	// 용어 수정 예정 임의로 작성
+	private User user;	// 	회원
+	private int replay;	//  음악플레이수
+	private int reply;	//  댓글 수
+	private int likes;	//  좋아요 수
+	
+}
