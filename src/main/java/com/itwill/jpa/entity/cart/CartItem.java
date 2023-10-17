b7b4a8cca052578b4a1731c00c301c8354eef7ba
qlsjsek2 <qlsjsek2@naver.com>
2023-10-17 19:09:00 +0900

    11

---
+++ src/main/java/com/itwill/jpa/entity/cart/CartItem.java
@@ -0,0 +1,26 @@
+package com.itwill.jpa.entity.cart;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.itwill.jpa.entity.product.Product;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class CartItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int cartItemNo;
+	private int cartNo;
+	private int cartItemQty;
+	private Product product;
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+}
