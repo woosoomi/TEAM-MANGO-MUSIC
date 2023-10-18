ca89eb73df82fe743d3aaa3730d5c2215138a411
차경진 <ckrudwls7@naver.com>
2023-10-18 10:19:45 +0900

    ...

--- src/main/java/com/itwill/jpa/dto/order/OrderDto.java
+++ src/main/java/com/itwill/jpa/dto/order/OrderDto.java
@@ -3,16 +3,20 @@ package com.itwill.jpa.dto.order;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.itwill.jpa.entity.order.Order;
 import com.itwill.jpa.entity.order.Order.OrderStatus;
 import com.itwill.jpa.entity.order.OrderItem;
+import com.itwill.jpa.entity.user.User;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 //웹에서 고객에게 보여주기 위한 정보를 담은 객체
 public class OrderDto {
 
@@ -26,17 +30,30 @@ public class OrderDto {
 
 	private OrderStatus orderstatus;
 
-	private List<OrderItem> orderItems; // 주문한 아이템 정보들(수량, 가격등)
+	private List<OrderItem> orderitems; // 주문한 아이템 정보들(수량, 가격등)
+
+	//고객 주문 정보의 초기화
+	public OrderDto(Order order) {
+		this.username = order.getUser().getUsername();
+		this.userphone = order.getUser().getUserphone();
+		this.useraddress = order.getUser().getUseraddress();
+		this.orderdate = order.getOrderdate();
+		this.orderstatus = order.getOrderstatus();
+		// 주문 아이템(OrderItem)을 OrderItemDto로 변환하여 리스트로 저장
+		//this.orderitems = order.getOrderitems().stream().map(OrderItemDto::fromOrderItem).collect(Collectors.toList());
+
+	}
 	
-	/*
-    // 멤버 필드 초기화 메서드(@AllArgsConstructor 쓰므로 생략)
-    public void initializeFields(String username, String userphone, String useraddress, LocalDateTime orderdate, OrderStatus orderstatus, List<OrderItem> orderItems) {
-        this.username = username;
-        this.userphone = userphone;
-        this.useraddress = useraddress;
-        this.orderdate = orderdate;
-        this.orderstatus = orderstatus;
-        this.orderItems = orderItems;
-    }
-	*/
+	//Dto -> Entity
+	public Order toEntity() {
+		return Order.builder()
+					.orderdate(orderdate)
+					.orderstatus(orderstatus)
+					.orderitems(orderitems)
+					.build();
+				
+	}
+	
 }
+			
+			
--- src/main/java/com/itwill/jpa/entity/order/Order.java
+++ src/main/java/com/itwill/jpa/entity/order/Order.java
@@ -18,12 +18,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "orders") //클래스 이름이 테이블명과 같지 않기 때문에 해당 어노테이션 추가
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 //데이터베이스에 들어갈 중요한 데이터를 포함한 객체
@@ -35,7 +37,7 @@ public class Order {
 	private LocalDateTime orderdate;
 	
 	@Enumerated(EnumType.STRING)
-	private OrderStatus status;
+	private OrderStatus orderstatus;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
