0610fa712f2199692e70b3568c556e0de427b7f2
차경진 <ckrudwls7@naver.com>
2023-10-18 14:34:44 +0900

    ...

--- src/main/java/com/itwill/jpa/dto/order/OrderItemDto.java
+++ src/main/java/com/itwill/jpa/dto/order/OrderItemDto.java
@@ -1,6 +1,37 @@
 package com.itwill.jpa.dto.order;
 
-public class OrderItemDto {
+import com.itwill.jpa.entity.order.OrderItem;
 
+public class OrderItemDto {
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//OrderDto에서 OrderItemDto를 쓰기위한 메서드
+	//(OrderItem entity를 건들지 않기위해 OrderItemDto를 대신해서 사용 = 데이터 무결성 유지 목적)
+	
+	/*
+	  
+    public static OrderItemDto fromOrderItem(OrderItem orderItem) {
+    
+        OrderItemDto dto = new OrderItemDto();
+        dto.setQuantity(orderItem.getQuantity());
+        dto.setPrice(orderItem.getPrice());
+        return dto;
+        
+    }
+    
+    */
 	
 }
