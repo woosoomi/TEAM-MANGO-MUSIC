b756d6498ab2e06e830021a455f502d62d405a0e
WooHanYeong <woocoding96@gmail.com>
2023-10-19 11:21:49 +0900

    11

---
+++ src/test/java/com/itwill/jpa/repository/user/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.itwill.jpa.repository.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
