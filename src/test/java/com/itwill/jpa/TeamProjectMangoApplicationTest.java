6fbac8da2ef33c7057ee6fd388997cf344326511
WooHanYeong <woocoding96@gmail.com>
2023-10-19 11:51:49 +0900

    1

---
+++ src/test/java/com/itwill/jpa/TeamProjectMangoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.itwill.jpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class TeamProjectMangoApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
