d02ea7a7d722ac3dd789bc829cf8ca2c13c37994
ggwanghun <ggwanghun@gmail.com>
2023-10-19 11:55:27 +0900

    231019 1155 product 음악 내림차순 출력기능 test 아직 안함

--- src/main/java/com/itwill/jpa/service/product/ProductServiceImpl.java
+++ src/main/java/com/itwill/jpa/service/product/ProductServiceImpl.java
@@ -1,21 +1,39 @@
 package com.itwill.jpa.service.product;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwill.jpa.dao.product.ProductDao;
 import com.itwill.jpa.entity.product.Product;
+import com.itwill.jpa.repository.product.ProductRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class ProductServiceImpl implements ProductService{
 	@Autowired
-	private ProductDao productDao;
+	private final ProductRepository productRepository;
+	private final ProductDao productDao;
 	
 
+	public ProductServiceImpl(ProductRepository productRepository, ProductDao productDao) {
+		this.productRepository = productRepository;
+		this.productDao = productDao;
+	}
+	
     public void saveProduct(Product product) {
         productDao.insertProduct(null);
     }
 
+    // ============================ 음악 내립차순 정렬 ============================//
+	@Override
+	public List<ProductDao> getAllMusicByProductNoDesc() {
+		return productDao.getAllMusicByProductNoDesc();
+	}
+	
 //    @Transactional
 //    public void updateProduct(Long productId, String name, int price) {
 //        Product findProduct = productRepository.findOneById(productId);
