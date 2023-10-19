ef1e68a77b34c84f021d4390313d37b0b267caf9
woosoomi <tnaldn01@gmail.com>
2023-10-19 17:26:08 +0900

    .

--- src/main/java/com/itwill/jpa/entity/product/Category.java
+++ src/main/java/com/itwill/jpa/entity/product/Category.java
@@ -7,9 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import com.itwill.jpa.entity.Board.Board;
 import com.itwill.jpa.entity.product.*;
-import com.itwill.jpa.entity.user.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -33,27 +31,28 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class Category {
-
-	@Id
-	@GeneratedValue
-	@Column(name = "category_id")
-	private Long categoryId;
-	private String categoryName;
-
-	/*
-	 * @OneToMany(mappedBy = "category",cascade = CascadeType.PERSIST) private
-	 * List<Product> products = new ArrayList<>();
-	 * 
-	 * 
-	 * @ManyToOne(fetch = FetchType.LAZY)
-	 * 
-	 * @JoinColumn(name = "parent_id") private Category parent;
-	 * 
-	 * @OneToMany(mappedBy = "parent") private List<Category> child = new
-	 * ArrayList<>();
-	 * 
-	 * //==연관관계 메서드==// public void addChildCategory(Category child) {
-	 * 
-	 * this.child.add(child); child.setParent(this); }
-	 */
+   
+ @Id @GeneratedValue
+ @Column(name = "category_id")
+ private Long categoryId;
+ private String categoryName;
+ 
+   @Builder.Default
+   @OneToMany(mappedBy = "category",cascade = CascadeType.PERSIST)
+   private List<Product> products  = new ArrayList();
+    
+ 
+/* @ManyToOne(fetch = FetchType.LAZY)
+ @JoinColumn(name = "parent_id")
+ private Category parent;
+ 
+ @OneToMany(mappedBy = "parent")
+ private List<Category> child = new ArrayList<>();
+ 
+ //==연관관계 메서드==//
+ public void addChildCategory(Category child) {
+    
+ this.child.add(child);
+ child.setParent(this);
+ }*/
 }
