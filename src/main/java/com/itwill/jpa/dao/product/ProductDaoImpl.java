6b47f023b741f07e3ba820b9b63cc62d0d442bc7
ggwanghun <ggwanghun@gmail.com>
2023-10-19 13:32:37 +0900

    dd

--- src/main/java/com/itwill/jpa/dao/product/ProductDaoImpl.java
+++ src/main/java/com/itwill/jpa/dao/product/ProductDaoImpl.java
@@ -1,14 +1,23 @@
 package com.itwill.jpa.dao.product;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
+import com.itwill.jpa.dto.product.ProductDto;
 import com.itwill.jpa.entity.product.Product;
+import com.itwill.jpa.entity.product.Product.Music;
+import com.itwill.jpa.repository.product.ProductRepository;
 
 @Repository
 public class ProductDaoImpl implements ProductDao{
-
+	private final ProductRepository productRepository;
+	
+	public ProductDaoImpl(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
 	@Override
 	public Product insertProduct(Product product) {
 		// TODO Auto-generated method stub
@@ -33,10 +42,18 @@ public class ProductDaoImpl implements ProductDao{
 		
 	}
 
+
+	private ProductDto convertToDto(Music music) {
+		ProductDto productDto = new ProductDto();
+		productDto.setProductNo(music.getProductNo());
+		productDto.setProductName(music.getProductName());
+		productDto.setReadCount(music.getReadCount());
+		return productDto;
+	}
+
 	@Override
 	public List<ProductDao> getAllMusicByProductNoDesc() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
