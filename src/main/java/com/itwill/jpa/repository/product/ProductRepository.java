55689891e41ffe66333d44d7f9dfb400299d961d
류광훈 <@14.35.67.171>
2023-10-23 03:40:56 +0900

    231023 0340 product

--- src/main/java/com/itwill/jpa/repository/product/ProductRepository.java
+++ src/main/java/com/itwill/jpa/repository/product/ProductRepository.java
@@ -2,31 +2,73 @@ package com.itwill.jpa.repository.product;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.itwill.jpa.entity.product.Product;
+import com.itwill.jpa.entity.product.Product.Goods;
+import com.itwill.jpa.entity.product.Product.Membership;
+import com.itwill.jpa.entity.product.Product.Music;
+import com.itwill.jpa.entity.product.Product.Ticket;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long>{
+@RequiredArgsConstructor
+public class ProductRepository{
 	
-//	private final EntityManager em;
-//	
-//    public void insert(Product product) {
-//        if(product.getProductId() == null) {
-//            em.persist(product);
-//        }else {
-//            em.merge(product);
-//        }
-//     }	
-//    public Product findOneById(Long id) {
-//        return em.find(Product.class, id);
-//    }
-//
-//    public List<Product> findAll() {
-//        return em.createQuery("select p from Product p",Product.class)
-//                .getResultList();
-//    }    
+	private final EntityManager em;
 	
+	public void insertProduct(Product product) {
+		if (product.getProductNo() == null) {
+			em.persist(product);
+		}else {
+			em.merge(product);
+		}
+	}
+/*Product 찾기*/
+	public Product findOne(Long productNo) {
+		return em.find(Product.class, productNo);
+	}
+	
+	public List<Product> findAll() {
+		return em.createQuery("select product from Product product", Product.class).getResultList();
+	}
+/*Music 찾기*/	
+	public Music findOneMusic(Long productNo) {
+		return em.find(Music.class, productNo);
+	}
+	
+	public List<Music> findAllMusic() {
+		return em.createQuery("select music from Music mudic", Music.class).getResultList();
+	}
+/*Goods 찾기*/	
+	public Goods findOneGoods(Long productNo) {
+		return em.find(Goods.class, productNo);
+	}
+	
+	public List<Goods> findAllGoods() {
+		return em.createQuery("select goods from Goods goods", Goods.class).getResultList();
+	}
+	
+/*Ticket 찾기*/
+	public Ticket findOneTicket(Long productNo) {
+		return em.find(Ticket.class, productNo);
+	}
+	
+	public List<Ticket> findAllTicket() {
+		return em.createQuery("select ticket from Ticket ticket", Ticket.class).getResultList();
+	}	
+/*Membership 찾기*/	
+	public Membership findOneMembership(Long productNo) {
+		return em.find(Membership.class, productNo);
+	}
+	
+	public List<Membership> findAllMembership() {
+		return em.createQuery("select membership from Membership membership", Membership.class).getResultList();
+	}	
 }
--- src/main/java/com/itwill/jpa/service/product/ProductService.java
+++ src/main/java/com/itwill/jpa/service/product/ProductService.java
@@ -4,11 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.itwill.jpa.entity.product.Product;
 import com.itwill.jpa.repository.product.ProductRepository;
 
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ProductService{
-	@Autowired
-	private ProductRepository productRepository;
+	
+	private final ProductRepository productRepository;
+	
+	@Transactional
+	public void insertProduct(Product product) {
+		productRepository.insertProduct(product);
+	}
+	
+	public List<Product> findProducts() {
+		return productRepository.findAll();
+	}
+	
+	public Product findOneProduct(Long productNo) {
+		return productRepository.findOne(productNo);
+	}
 }
