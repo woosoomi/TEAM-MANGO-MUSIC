f7ba0eff3ac3716f9c95f4b74404f7fc97b70320
차경진 <ckrudwls7@naver.com>
2023-10-23 15:44:27 +0900

    ...

--- src/main/java/com/itwill/jpa/service/order/OrderService.java
+++ src/main/java/com/itwill/jpa/service/order/OrderService.java
@@ -1,5 +1,20 @@
 package com.itwill.jpa.service.order;
 
+import java.util.List;
+
+import com.itwill.jpa.entity.order.Order;
+
 public interface OrderService {
+	//주문 생성
+	Order saveOrder(Order order);
+	//주문 정보 수정
+	Order updateOrder(Order order) throws Exception;
+	//주문 한개 삭제
+	void deleteOrder(Long orderId) throws Exception;
+	//주문 전체 삭제
+	void deleteAllOrder() throws Exception;
+	//유저 Id로 전체 주문 불러오기
+	List<Order> OrdersByUserId(String UserId);
+
 
 }
--- src/main/java/com/itwill/jpa/service/order/OrderServiceImpl.java
+++ src/main/java/com/itwill/jpa/service/order/OrderServiceImpl.java
@@ -1,5 +1,43 @@
 package com.itwill.jpa.service.order;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itwill.jpa.entity.order.Order;
+import com.itwill.jpa.repository.order.OrderRepository;
+
 public class OrderServiceImpl implements OrderService{
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	@Override
+	public Order saveOrder(Order order) {
+		return orderRepository.save(order);
+	}
+
+	@Override
+	public Order updateOrder(Order order) throws Exception{
+		return null;
+	}
+
+	@Override
+	public void deleteOrder(Long orderId) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllOrder() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Order> OrdersByUserId(String UserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
