f3e8924851f7370af86e36d3ca1b6b7fadb6b4ac
qlsjsek2 <qlsjsek2@naver.com>
2023-10-23 19:34:08 +0900

    1

--- src/main/java/com/itwill/jpa/repository/cart/CartItemRepository.java
+++ src/main/java/com/itwill/jpa/repository/cart/CartItemRepository.java
@@ -8,5 +8,4 @@ import com.itwill.jpa.entity.cart.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long>{
 	 //List<CartItem> getCartItemsByUserId(String userId);
-	 void deleteByUserIdAndCartItemId(String userId, Long cartItemId);
 }
