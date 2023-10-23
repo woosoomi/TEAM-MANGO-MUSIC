4a1f6e377652a2afd4f2052046ab0e414ea925be
qlsjsek2 <qlsjsek2@naver.com>
2023-10-23 16:15:03 +0900

    insert문 작성 완료, service 구현중

--- src/main/java/com/itwill/jpa/service/cart/CartService.java
+++ src/main/java/com/itwill/jpa/service/cart/CartService.java
@@ -1,8 +1,22 @@
 package com.itwill.jpa.service.cart;
 
+import java.util.List;
+
 import com.itwill.jpa.entity.cart.Cart;
 
 public interface CartService {
-	public Cart insert(Cart cart);
+	
+	//public Cart insert(Cart cart);
+	
+	//장바구니 비우기
+	public void deleteAll(List<Cart> cartList) throws Exception;
+	
+	//총합 계산
+	public double calculateTotalPrice(Cart cart) throws Exception;
+	
+	//장바구니 조회
+	List<Cart> getCartItems(Cart cart) throws Exception;
+	
+	
 	
 }
--- src/main/java/com/itwill/jpa/service/cart/CartServiceImpl.java
+++ src/main/java/com/itwill/jpa/service/cart/CartServiceImpl.java
@@ -1,9 +1,49 @@
 package com.itwill.jpa.service.cart;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.itwill.jpa.entity.cart.Cart;
+import com.itwill.jpa.entity.cart.CartItem;
+import com.itwill.jpa.entity.user.User;
+import com.itwill.jpa.repository.cart.CartRepository;
+import com.itwill.jpa.repository.user.UserRepository;
 
-public class CartServiceImpl {
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class CartServiceImpl implements CartService {
+	@Autowired
+	CartRepository cartRepository;
+	
+	@Override
+	public void deleteAll(List<Cart> cartList) {
+		Cart cart = new Cart();
+		
+		cartRepository.deleteAllById(null);
+	}
+
+	@Override
+	public double calculateTotalPrice(Cart cart) {
+		double total = 0.0;
+        List<CartItem> cartItems = cart.getCartitems();
+
+        for (CartItem cartItem : cartItems) {
+            double itemPrice = cartItem.getProduct().getProductPrice();
+            int quantity = cartItem.getCartItemQty();
+            total += itemPrice * quantity;
+        }
+
+        return total;
+	}
+
+	@Override
+	public List<Cart> getCartItems(Cart cart) {
+		return cartRepository.findAll();
+	}
 	
 }
