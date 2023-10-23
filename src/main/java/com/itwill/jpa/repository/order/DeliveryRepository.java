480935d9f2e9c23a84c9df7d1f63bb66937068e9
차경진 <ckrudwls7@naver.com>
2023-10-23 12:23:48 +0900

    ...이제 order쪽 서비스 할겁니다

--- src/main/java/com/itwill/jpa/repository/order/DeliveryRepository.java
+++ src/main/java/com/itwill/jpa/repository/order/DeliveryRepository.java
@@ -1,17 +1,12 @@
 package com.itwill.jpa.repository.order;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itwill.jpa.entity.order.Delivery;
-import com.itwill.jpa.entity.order.Order;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, Long>{
 
-<<<<<<< HEAD
+
 //	List<Delivery> findByUserId(String userId);
-=======
-	//List<Delivery> findByUserId(String userId);
->>>>>>> branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team1-mango.git
+
 }
