c9ef1e89e837c66e893d5c5cd1f877601147d420
rhqkf7496 <rhqkf7496@naver.com>
2023-10-24 18:53:20 +0900

    user ui 살짝 손댐...

--- src/main/java/com/itwill/jpa/controller/user/UserController.java
+++ src/main/java/com/itwill/jpa/controller/user/UserController.java
@@ -1,8 +1,10 @@
 package com.itwill.jpa.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserController {
 	
 	/*
