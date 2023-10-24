438e03823750328705f3128d77315061fceeb0d5
WooHanYeong <woocoding96@gmail.com>
2023-10-24 14:47:34 +0900

    cart test ing....

--- src/main/java/com/itwill/jpa/controller/board/BoardController.java
+++ src/main/java/com/itwill/jpa/controller/board/BoardController.java
@@ -5,18 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.jpa.entity.board.Board;
 import com.itwill.jpa.service.board.BoardServiceImpl;
 
-import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 
-//@Controller
-//@RequestMapping("/board")
-//@RequiredArgsConstructor //Lombok 라이브러리
-//public class BoardController {
-//	
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor //Lombok 라이브러리
+public class BoardController {
+	
 //	private BoardServiceImpl boardServiceImpl;
 //	
 //    @GetMapping("/events")
@@ -26,4 +28,4 @@ import lombok.RequiredArgsConstructor;
 //        return "board/event"; 
 //    }
 //	//미완성
-//}
+}
