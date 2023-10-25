2d20b72175d43e8a74b30f078792bbf24130e085
silvia32849 <silvia32849@naver.com>
2023-10-25 17:06:01 +0900

    ..

--- src/main/java/com/itwill/jpa/controller/order/OrderController.java
+++ src/main/java/com/itwill/jpa/controller/order/OrderController.java
@@ -1,9 +1,13 @@
 package com.itwill.jpa.controller.order;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.itwill.jpa.entity.order.Order;
 import com.itwill.jpa.service.order.OrderService;
 
 @Controller
@@ -17,7 +21,16 @@ public class OrderController {
 		return "order";
 	}
 	@GetMapping("/orderdetail")
-	public String orderdetail_page() {
+	public String orderdetail_page(Model model) {
+		try {
+		List<Order> orderList = orderService.orderListByNewer("why3795");
+		model.addAttribute(orderList);
+		System.out.println("주문내역:" +orderList);
 		return "orderdetail";
+		}catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("erroMsg: "+ e.getMessage());
+			return "index";
+		}
 	}
 }
