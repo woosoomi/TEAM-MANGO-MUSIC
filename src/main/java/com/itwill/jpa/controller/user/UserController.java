d4db805dd2d2550870ed0b598ba78f1fb80b6376
rhqkf7496 <rhqkf7496@naver.com>
2023-10-25 16:25:52 +0900

    user ui 수정중

--- src/main/java/com/itwill/jpa/controller/user/UserController.java
+++ src/main/java/com/itwill/jpa/controller/user/UserController.java
@@ -2,6 +2,7 @@ package com.itwill.jpa.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.itwill.jpa.entity.user.User;
@@ -11,16 +12,28 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-    
-	
-    }
 
+	@GetMapping("/userJoinForm")
+	public String userJoinForm() {
+		String forwardPath = "userJoinForm";
+		return forwardPath;
+
+	}
 	
+	@GetMapping("/userCheckId")
+	public String userCheckId() {
+		String forwardPath = "userCheckId";
+		return forwardPath;
+		
+	}
 	
-	
-	
-	
+	@GetMapping("/userCheckPw")
+	public String userCheckPw() {
+		String forwardPath = "userCheckPw";
+		return forwardPath;
+		
+	}
+}
