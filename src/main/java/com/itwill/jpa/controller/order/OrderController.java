01599575cf7112cfa25e812aa342742cbbf89ce0
silvia32849 <silvia32849@naver.com>
2023-10-25 11:31:14 +0900

    order랑 orderdetail페이지 생성, order 매핑함

--- src/main/java/com/itwill/jpa/controller/order/OrderController.java
+++ src/main/java/com/itwill/jpa/controller/order/OrderController.java
@@ -1,8 +1,23 @@
 package com.itwill.jpa.controller.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.itwill.jpa.service.order.OrderService;
 
 @Controller
 public class OrderController {
 
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping("/order")
+	public String order_page() {
+		return "order";
+	}
+	@GetMapping("/orderdetail")
+	public String orderdetail_page() {
+		return "orderdetail";
+	}
 }
