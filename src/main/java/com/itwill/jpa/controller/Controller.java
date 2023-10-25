10ce32e592004576714d1e5d734cb5a82efd4034
rhqkf7496 <rhqkf7496@naver.com>
2023-10-25 20:33:43 +0900

    user ui 살짝 수정했습니다..

--- src/main/java/com/itwill/jpa/controller/Controller.java
+++ src/main/java/com/itwill/jpa/controller/Controller.java
@@ -4,24 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-	/*
-	@GetMapping("/index")
-	public String index() {
-		String forwardPath = "index";
-		return forwardPath;
-	}
-	*/
-
+	
 	@GetMapping("/userprofile")
 	public String userprofile() {
 		String forwardPath = "userprofile";
-		return forwardPath;
-
-	}
-
-	@GetMapping("/userJoinForm")
-	public String userJoinForm() {
-		String forwardPath = "userJoinForm";
 		return forwardPath;
 
 	}
--- src/main/java/com/itwill/jpa/controller/user/UserController.java
+++ src/main/java/com/itwill/jpa/controller/user/UserController.java
@@ -4,26 +4,57 @@ import com.itwill.jpa.entity.user.User;
 import com.itwill.jpa.service.user.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
     
-    @GetMapping("/index")
+    @RequestMapping("/index")
     public String main() {
     	String forwardPath = "index";
     	return forwardPath;
     }
     
+    @RequestMapping("/userJoinForm")
+	public String userJoinForm() {
+		String forward_path = "userJoinForm";
+		return forward_path;
+	}
+    
+    @RequestMapping("/userCheckIdPw")
+	public String userCheckIdPw() {
+		String forward_path = "userCheckIdPw";
+		return forward_path;
+	}
+    
+    @PostMapping("user_write_action")
+	public String user_write_action(@ModelAttribute("fuser") User user, Model model) throws Exception {
+		String forward_path = "";
+		try {
+			User rowCount = userService.createUser(user);
+			forward_path="redirect:user_login_form";
+		}catch (Exception e) {
+			model.addAttribute("msg", e.getMessage());
+			model.addAttribute("fuser", user);
+			forward_path="user_write_form";
+		}
+		return forward_path;
+	}
+    
+    @RequestMapping("/user_login_form")
+	public String user_login_form() {
+		String forward_path = "user_login_form";
+		return forward_path;
+	}
     
     
     
