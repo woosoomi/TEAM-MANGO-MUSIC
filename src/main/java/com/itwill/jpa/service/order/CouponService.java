1d3b810c951c76b53f3fc20df4ee9c6894431eca
차경진 <ckrudwls7@naver.com>
2023-10-26 17:44:44 +0900

    OrderService, CouponService entity 사용을 dto 사용으로 대체하는 작업중

--- src/main/java/com/itwill/jpa/service/order/CouponService.java
+++ src/main/java/com/itwill/jpa/service/order/CouponService.java
@@ -2,21 +2,21 @@ package com.itwill.jpa.service.order;
 
 import java.util.List;
 
-import com.itwill.jpa.entity.order.Coupon;
+import com.itwill.jpa.dto.order.CouponDto;
 
 public interface CouponService {
 	
 	//쿠폰생성
-	Coupon saveCoupon(Coupon coupon);
+	CouponDto saveCoupon(CouponDto couponDto);
 	//쿠폰수정(관리자)
-	Coupon updateCoupon(Coupon coupon) throws Exception;
+	CouponDto updateCoupon(CouponDto couponDto) throws Exception;
 	//쿠폰 삭제
 	void deleteCoupon(Long couponId) throws Exception;
 	//쿠폰 전체 삭제
 	void deleteAllCoupons() throws Exception;
 	//유저의 쿠폰들 불러오기
-	List<Coupon> couponsByUserId(String userId);
+	List<CouponDto> couponsByUserId(String userId);
 	//주문내역에서 해당 주문에 사용된 쿠폰 불러오기
-	Coupon findCouponByOrderId(Long orderId);
+	CouponDto findCouponByOrderId(Long orderId);
 	
 }
