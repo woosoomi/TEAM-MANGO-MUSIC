02ce24e9dde95d9445ed5110d8589fb3929af181
qlsjsek2 <qlsjsek2@naver.com>
2023-10-27 17:39:06 +0900

    ..

--- src/main/resources/Sql/cart-insert.sql
+++ src/main/resources/Sql/cart-insert.sql
@@ -7,14 +7,14 @@ insert into cart (cart_id, cart_tot_price, user_id) values (Cart_Cart_no_seq.nex
 insert into cart (cart_id, cart_tot_price, user_id) values (Cart_Cart_no_seq.nextval, 7000, 'ycl77');
 insert into cart (cart_id, cart_tot_price, user_id) values (Cart_Cart_no_seq.nextval, 8000, 'wsm55');
 
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,642,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,643,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,644,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,645,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,646,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,647,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,648,1);
-insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,649,1);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,10,1,1);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,9,2,2);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,8,3,3);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,7,4,4);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,5,5,5);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,2,6,6);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,6,7,7);
+insert into cart_item(cart_item_id,cart_item_qty,cart_no,product_no) values (cart_item_no_seq.nextval,1,8,8);
 
 
 
