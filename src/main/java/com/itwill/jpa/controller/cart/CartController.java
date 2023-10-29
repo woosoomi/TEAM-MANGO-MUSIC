48052ebb9d44f7af1a4022a8c0e18cb61d84b3f0
WooHanYeong <woocoding96@gmail.com>
2023-10-30 01:54:56 +0900

    cart

--- src/main/java/com/itwill/jpa/controller/cart/CartController.java
+++ src/main/java/com/itwill/jpa/controller/cart/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,7 +19,13 @@ import com.itwill.jpa.service.cart.CartServiceImpl;
 
 
 @Controller
+@RequestMapping("/")
+
 public class CartController {
 
-	
+	@GetMapping("/cart") 
+	public String cart() { 
+		String forward_path = "cart"; 
+		return forward_path; 
+	}
 }
