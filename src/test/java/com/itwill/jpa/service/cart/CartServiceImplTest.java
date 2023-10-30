e5731130a6f384211009ebc24bae000943f0361a
qlsjsek2 <qlsjsek2@naver.com>
2023-10-30 13:37:49 +0900

    123

--- src/test/java/com/itwill/jpa/service/cart/CartServiceImplTest.java
+++ src/test/java/com/itwill/jpa/service/cart/CartServiceImplTest.java
@@ -52,10 +52,13 @@ class CartServiceImplTest extends TeamProjectMangoApplicationTest {
 		if (findUser.isPresent()) {
 			User user = findUser.get();
 			System.out.println("user>>>>>>>>>>>>>>>>>>"+user);
-			CartDto cart1 = CartDto.builder().cartId(0L).cartTotPrice(1000).userId(user.getUserId()).build();
-			System.out.println("cart1>>>>>>>>>>>>>>>>"+cart1);
-			CartDto insertedCart=cartServiceImpl.createCart(cart1);
-			System.out.println("insert>>>>>>>>>>>>>>>>>>>>>"+insertedCart);
+			CartDto cartDto = new CartDto();
+			cartDto.setCartId(0L);
+			cartDto.setUserId(user.getUserId());
+			cartDto.setCartTotPrice(10);
+			System.out.println("cart1>>>>>>>>>>>>>>>>"+cartDto);
+			CartDto createCart = cartServiceImpl.createCart(cartDto);
+			System.out.println("insert>>>>>>>>>>>>>>>>>>>>>"+createCart);
 		}
 
 	}
