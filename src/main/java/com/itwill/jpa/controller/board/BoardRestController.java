f86c6bc0cb0b665998c0a0fabfe50bb4643ceaef
WooHanYeong <woocoding96@gmail.com>
2023-10-30 18:07:21 +0900

    레컨

--- src/main/java/com/itwill/jpa/controller/board/BoardRestController.java
+++ src/main/java/com/itwill/jpa/controller/board/BoardRestController.java
@@ -2,8 +2,9 @@ package com.itwill.jpa.controller.board;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -32,34 +33,8 @@ public class BoardRestController {
 	@Autowired
 	private BoardServiceImpl boardServiceImpl;
 	
-	
-	
-	
-	@Operation(summary = "삭제[cascade처리]")
-	@DeleteMapping("/{boardId}")
-	public void BoardDelete(@PathVariable(name="boardId") Long boardId) {
-		boardServiceImpl.delete(boardId);
-	}
-	
-	@Operation(summary = "오래된 순서로 정렬[성공]")
-	@GetMapping("/reverseSort")
-	public ResponseEntity<List<BoardDto>> getSortData(@RequestParam(name="no") Long categoryId, @RequestParam(name="id") String userId) {
-		List<Board> boardList = boardServiceImpl.findByBoardCategory_IdAndUser_UserIdOrderByCreatedTime(categoryId, userId);
-		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
-		Collections.reverse(boardDtoList);
-		
-	    for (Board board : boardList) {
-	        boardDtoList.add(BoardDto.toDto(board));
-	    }
-		
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-		
-		return new ResponseEntity<>(boardDtoList,httpHeaders,HttpStatus.OK);
-	}
-	
-	@Operation(summary="게시판등록")
-	@PostMapping
+	@Operation(summary="게시판등록[성공]")
+	@PostMapping("/{boardId}")
 	public ResponseEntity<BoardDto> createBoard(@RequestBody BoardDto boardDto) {
 	    try {
 	        // BoardDto를 엔티티로 변환하여 서비스에서 사용
@@ -74,8 +49,60 @@ public class BoardRestController {
 	        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 	}
+	
+	
+	@Operation(summary = "삭제[성공]")
+	@DeleteMapping("/{boardId}")
+	public void BoardDelete(@PathVariable(name="boardId") Long boardId) {
+		boardServiceImpl.delete(boardId);
+	}
+	
+	
+///////////////////////////// 공지사항 ///////////////////////////////////////////////	
+	
+	
+///////////////////////////// 매거진 ///////////////////////////////////////////////	
+///////////////////////////// faq ///////////////////////////////////////////////	
+///////////////////////////// 1대1문의 ///////////////////////////////////////////////	
+	
+	
+	@Operation(summary = "1대1문의 오래된 순서로 정렬[성공]")
+	@GetMapping("/inquiries/reverseSort")
+	public ResponseEntity<List<BoardDto>> reverseSortInquiries(@RequestParam(name="no") Long categoryId, @RequestParam(name="id") String userId) {
+		List<Board> boardList = boardServiceImpl.findByBoardCategory_IdAndUser_UserIdOrderByCreatedTime(categoryId, userId);
+		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
+		
+	    for (Board board : boardList) {
+	        boardDtoList.add(BoardDto.toDto(board));
+	    }
+		
+	    HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+		
+		return new ResponseEntity<>(boardDtoList,httpHeaders,HttpStatus.OK);
+	}
+	
+	@Operation(summary = "1대1문의 최신로 정렬[성공]")
+	@GetMapping("/inquiries/sort")
+	public ResponseEntity<List<BoardDto>> sortInquiries(@RequestParam(name="no") Long categoryId, @RequestParam(name="id") String userId) {
+	    List<Board> boardList = boardServiceImpl.findByBoardCategory_IdAndUser_UserIdOrderByCreatedTime(categoryId, userId);
+	    //최신순서로 정렬하는 코딩
+	    List<BoardDto> boardDtoList = boardList.stream()
+	        .map(BoardDto::toDto) 
+	        .sorted(Comparator.comparing(BoardDto::getCreatedTime).reversed()) 
+	        .collect(Collectors.toList());
 
+	    HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
+	    return new ResponseEntity<>(boardDtoList, httpHeaders, HttpStatus.OK);
+	}
+
+	
+	
+	
+
+///////////////////////////// 이벤트 ///////////////////////////////////////////////	
 
 	
 	
