b15db6107997247f8cef88cb463902dec83f6e98
silvia32849 <silvia32849@naver.com>
2023-11-02 13:20:14 +0900

    구매날짜, 구매가격, 주문상태 추가 성공!!!!

--- src/main/java/com/itwill/jpa/dto/order/OrderDto.java
+++ src/main/java/com/itwill/jpa/dto/order/OrderDto.java
@@ -1,7 +1,11 @@
 package com.itwill.jpa.dto.order;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.itwill.jpa.entity.order.Order;
 import com.itwill.jpa.entity.order.Order.OrderStatus;
@@ -35,6 +39,11 @@ public class OrderDto {
 	@Builder.Default
 	private List<OrderItemDto> orderItemDtos = new ArrayList<>();
 	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
 	
 	//Dto에서 고객에게 보여주는 주문 정보들이 어떤값인지를 설정하는 생성자(초기화)
 	public OrderDto(Order order) {
@@ -44,7 +53,8 @@ public class OrderDto {
 		this.orderStatus = order.getOrderStatus();
 		this.userId = order.getUser().getUserId();
 		this.deliveryId = order.getDelivery().getDeliveryId();
-		
+		this.createdAt = order.getCreatedAt();
+		this.updatedAt = order.getUpdatedAt();
 	}
 	
 	//Entity to Dto 변환
@@ -55,6 +65,8 @@ public class OrderDto {
 									.orderStatus(entity.getOrderStatus())
 									.userId(entity.getUser().getUserId())
 									.deliveryId(entity.getDelivery().getDeliveryId())
+									.createdAt(entity.getCreatedAt())
+									.updatedAt(entity.getUpdatedAt())
 									.build();
 		
 		// OrderItem 엔티티 목록을 OrderItemDto 목록으로 변환하여 설정
--- src/main/resources/Sql/orders-insert.sql
+++ src/main/resources/Sql/orders-insert.sql
@@ -94,67 +94,67 @@ INSERT INTO ORDER_ITEM (oi_qty, oi_id, order_id, product_no) values (2, order_it
 
 --test data 9(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 관악구', 'CJ대한통운', '우한영', '010-3333-4442', 'why3795');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 관악구', 'CJ대한통운', '우한영', '010-3333-4442', 'why3795');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
--- values (80000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'why3795');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
+values (80000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'why3795');
 
--- --test data 10(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 10(ALL DATA EXCEPT ORDER_ITEM,COUPON)
  
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 성동구', '우체국택배', '차경진', '011-2918-3324', 'cgj22');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 성동구', '우체국택배', '차경진', '011-2918-3324', 'cgj22');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
--- values (50000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'cgj22');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
+values (50000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'cgj22');
 
 
--- --test data 11(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 11(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 구로구', '한진택배', '이승규', '011-3503-3404', 'lsg33');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 구로구', '한진택배', '이승규', '011-3503-3404', 'lsg33');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
--- values (40000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '결제완료', 'lsg33');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
+values (40000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '결제완료', 'lsg33');
 
--- --test data 12(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 12(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 송파구', '롯데택배', '문예린', '011-6545-5489', 'myr1109');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 송파구', '롯데택배', '문예린', '011-6545-5489', 'myr1109');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
--- values (30000,SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'myr1109');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
+values (30000,SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'myr1109');
 
--- --test data 13(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 13(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 노원구', '로젠택배', '우수미', '011-4399-3249', 'wsm55');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 노원구', '로젠택배', '우수미', '011-4399-3249', 'wsm55');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
--- values (10000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'wsm55');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
+values (10000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'wsm55');
 
--- --test data 14(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 14(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 양천구', '우체국택배', '마동석', '011-2304-3498', 'rgh66');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 양천구', '우체국택배', '마동석', '011-2304-3498', 'rgh66');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
--- values (70000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송준비중', 'rgh66');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
+values (70000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송준비중', 'rgh66');
 
--- --test data 15(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 15(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 강동구', 'CJ대한통운', '유채린', '011-3485-3490', 'ycl77');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 강동구', 'CJ대한통운', '유채린', '011-3485-3490', 'ycl77');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
--- values (60000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'ycl77');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id)  
+values (60000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송중', 'ycl77');
 
--- --test data 16(ALL DATA EXCEPT ORDER_ITEM,COUPON)
+--test data 16(ALL DATA EXCEPT ORDER_ITEM,COUPON)
 
--- INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
--- values (delivery_no_seq.nextval, '서울시 강남구', '로젠택배', '개리', '011-1432-2355', 'kbs88');
+INSERT INTO DELIVERY (DELIVERY_ID, DELIVERY_ADDRESS, DELIVERY_COMPANY, DELIVERY_NAME, DELIVERY_PHONE, USER_ID) 
+values (delivery_no_seq.nextval, '서울시 강남구', '로젠택배', '개리', '011-1432-2355', 'kbs88');
 
--- INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
--- values (80000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'kbs88');
+INSERT INTO ORDERS (order_price, created_at, delivery_id, order_id, updated_at, order_status, user_id) 
+values (80000, SYSTIMESTAMP, delivery_no_seq.currval, order_no_seq.nextval, SYSTIMESTAMP, '배송완료', 'kbs88');
 
 commit;
