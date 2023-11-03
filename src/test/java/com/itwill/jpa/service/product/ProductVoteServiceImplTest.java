66bfa9fe3a599a3673bd8cae76eb9b730288d10f
유채린 <cofls000@naver.com>
2023-11-03 10:27:51 +0900

    voteCss 수정

--- src/test/java/com/itwill/jpa/service/product/ProductVoteServiceImplTest.java
+++ src/test/java/com/itwill/jpa/service/product/ProductVoteServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.itwill.jpa.service.product;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.jpa.TeamProjectMangoApplicationTest;
 import com.itwill.jpa.dao.product.ProductVoteDaoImpl;
+import com.itwill.jpa.dto.product.ProductVoteDto;
 import com.itwill.jpa.entity.product.Product;
 import com.itwill.jpa.entity.vote.Vote;
 import com.itwill.jpa.repository.product.ProductRepository;
@@ -30,12 +33,13 @@ class ProductVoteServiceImplTest extends TeamProjectMangoApplicationTest{
 	@Test
 	@Transactional
 	@Rollback(false)
-	@Disabled
+	//@Disabled
 	// 상품에서 음악 조회수(readCount), 음악 별점(productStar)의 합산해서 Top 20명 추출
 	public void testfindTop20ByTotalScore() {
 
-	System.out.println(">>>>>>>>>>" + productVoteServiceImple.findbyTop20ByTotalScore());
-		
+	//System.out.println(">>>>>>>>>>" + productVoteServiceImple.findbyTop20ByTotalScore());
+		List<ProductVoteDto> productVote = productVoteDaoImpl.findProductsByVoteIsNotNullOrderByVoteTotDesc();
+		System.out.println(productVote);	
 	}
 
 	
