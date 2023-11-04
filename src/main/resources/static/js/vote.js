38bd4b30a64c9239f55375848558713b823df027
유채린 <@MarianaYou>
2023-11-04 09:50:05 +0900

    투표 남은시간 카운트다운 제작

--- src/main/resources/static/js/vote.js
+++ src/main/resources/static/js/vote.js
@@ -1,6 +1,6 @@
 
 
-
+// AJAX 요청 미구현 추후 구현 예정
 
 function voteButtonClicked(voteId) {
     // AJAX 요청을 보냅니다.
@@ -19,3 +19,34 @@ function voteButtonClicked(voteId) {
         }
     });
 }
+
+
+
+
+
+
+// endDate는 추후 VoteDate의 데이터로 변경예정
+var endDate = new Date('2023-11-15T23:59:59').getTime();
+
+        // 카운트다운 업데이트 함수
+        function updateCountdown() {
+            var now = new Date().getTime();
+            var timeLeft = endDate - now;
+
+            if (timeLeft < 0) {
+                document.getElementById('countdown-timer').innerHTML = "투표종료";
+            } else {
+                var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+                document.getElementById('days').innerHTML = days;
+                document.getElementById('hours').innerHTML = hours;
+                document.getElementById('minutes').innerHTML = minutes;
+                document.getElementById('seconds').innerHTML = seconds;
+            }
+        }
+
+        // 매 초마다 카운트다운 업데이트
+        var countdownInterval = setInterval(updateCountdown, 1000);
