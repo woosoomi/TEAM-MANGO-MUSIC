6b3b4d3d03234c915eda40df5605d189b095733c
qlsjsek2 <qlsjsek2@naver.com>
2023-11-06 12:15:28 +0900

    삭제,수량 업데이트 작업중

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
-	$('#selectAll').change(function() {
-		var checkboxes = document.getElementsByClassName('checkbox');
-		for (var i = 0; i < checkboxes.length; i++) {
-			checkboxes[i].checked = this.checked;
-		}
-	});
-
-$('.delete-btn').on("click", (function() {
-    var cartItemId = $(this).data('cart-item-id'); // data-cart-item-id의 값을 가져옴
-    $.ajax({
-        url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart/' + cartItemId,
-        type: 'DELETE',
-        success: function(response) {
-            console.log('삭제 성공:', response);
-        },
-        error: function(error) {
-            console.error('삭제 실패:', error);
+    // 전체 선택 체크박스가 변경될 때 실행되는 부분
+    $('#selectAll').change(function() {
+        var checkboxes = document.getElementsByClassName('checkbox');
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = this.checked;
         }
     });
-}));
+    // 삭제 버튼 클릭 시 실행되는 부분
+    $('.delete-btn').click(function() {
+        var checkedItems = document.querySelectorAll('.checkbox:checked');
+        var cartItemIds = Array.from(checkedItems).map(item => item.dataset.cartItemId);
 
-document.addEventListener('DOMContentLoaded', function(e) {
-	var cartId = 1;
-	// 페이지가 로드될 때 실행되는 부분
-	calculateTotalPrice(cartId);
-	// 총 가격을 업데이트하는 함수
-	function calculateTotalPrice(cartId) {
-		console.log(cartId);
-		$.ajax({
-			url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart/' + cartId,
-			type: 'GET',
-			success: function(data) {
-				document.getElementById('cartTotPrice').innerHTML = data.cartTotPrice + '원';
-				console.log(document.getElementById('cartTotPrice'));
-			},
-			error: function(error) {
-				console.error('Error:', error);
-			}
-		});
-		e.preventDefault();
-	}
-});
+        // AJAX 요청을 사용하여 서버에 삭제 요청을 보냅니다.
+        $.ajax({
+            url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart',
+            type: 'DELETE',
+            data: JSON.stringify(cartItemIds),
+            success: function(response) {
+                console.log('삭제 성공:', response);
+                // 여기에서 삭제된 상품들을 화면에서 숨기거나 제거하는 작업을 할 수 있습니다.
+            },
+            error: function(error) {
+                console.error('삭제 실패:', error);
+            }
+        });
+    });
+
+    // 페이지가 로드될 때 실행되는 부분
+    var cartId = 1;
+    calculateTotalPrice(cartId);
+
+    // 총 가격을 업데이트하는 함수
+    function calculateTotalPrice(cartId) {
+        console.log(cartId);
+        $.ajax({
+            url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart/' + cartId,
+            type: 'GET',
+            success: function(data) {
+                document.getElementById('cartTotPrice').innerHTML = data.cartTotPrice + '원';
+                console.log(document.getElementById('cartTotPrice'));
+            },
+            error: function(error) {
+                console.error('Error:', error);
+            }
+        });
+    }
 });
