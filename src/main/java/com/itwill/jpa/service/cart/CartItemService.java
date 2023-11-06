6cef4d3d51bdb3193c7d921f8dd491f9cfbd382b
qlsjsek2 <qlsjsek2@naver.com>
2023-11-06 19:17:34 +0900

    삭제기능 완료!

--- src/main/java/com/itwill/jpa/service/cart/CartItemService.java
+++ src/main/java/com/itwill/jpa/service/cart/CartItemService.java
@@ -19,6 +19,8 @@ public interface CartItemService {
 	//상품 삭제
 	public void deleteByCartItemId(Long cartItemId) throws Exception;
 	
+	public void deleteByCartItemIds(List<Long> cartItemIds) throws Exception;
+	
 	//장바구니에 담긴 모든 상품
 	public List<CartItemDto> findAllByCartId(Long cartId) throws Exception;
 	
