63d2f58f7696306c42fb647a29b7c18cf1e08e41
woosoomi <tnaldn01@gmail.com>
2023-11-07 12:08:47 +0900

    뮤직 리스트에서 일정량 스크롤시 top 버튼 - 작동성공 Merge branch 'master1' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team1-mango.git into master1

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -29,3 +29,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+$(function() { // 보이기 | 숨기기
+	$(window).scroll(function() {
+    if ($(this).scrollTop() > 250) { //250 넘으면 버튼이 보여짐
+      $('#topBtn').fadeIn();
+    } else {
+      $('#topBtn').fadeOut();
+    }
+  }); // 버튼 클릭시 
+  $("#topBtn").click(function() { 
+  	$('html, body').animate({ scrollTop : 0 // 0 까지 animation 이동
+  	}, 400); // 속도 400 
+  	return false; 
+  }); 
+});
+
