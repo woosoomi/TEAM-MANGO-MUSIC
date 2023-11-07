aae78f02acc55024d727a81faed162f965d212ea
차경진 <ckrudwls7@naver.com>
2023-11-07 13:57:48 +0900

    카트 결제 작업중

--- src/main/java/com/itwill/jpa/controller/cart/CartController.java
+++ src/main/java/com/itwill/jpa/controller/cart/CartController.java
@@ -8,13 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.itwill.jpa.dto.cart.CartDto;
 import com.itwill.jpa.dto.cart.CartItemDto;
 import com.itwill.jpa.dto.product.ProductDto;
 import com.itwill.jpa.service.cart.CartItemService;
 import com.itwill.jpa.service.cart.CartService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -25,8 +27,16 @@ public class CartController {
 	CartService cartService;
 
 	@GetMapping("/cart_main")
-	public String cart(Model model) {
+	public String cart(Model model, HttpServletRequest request) {
 	    try {
+	    	
+	    	/*오더의 손길*/
+			//임의로 세션 로그인 유저 설정함
+			HttpSession session = request.getSession();
+			session.setAttribute("user_id", "lsg33");
+			String userId = (String) session.getAttribute("user_id");
+			model.addAttribute("user_id", userId);
+	    	
 	        CartDto cart = cartService.findCartByCartId(1L);
 	        List<CartItemDto> cartItems = cartItemService.findAllByCartId(cart.getCartId());
 	        
