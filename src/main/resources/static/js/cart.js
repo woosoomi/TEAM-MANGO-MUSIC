fc5c0ac0bfc9097ba38f16fb38393d6a5e39fe02
qlsjsek2 <qlsjsek2@naver.com>
2023-11-09 18:30:27 +0900

    총 주문금액 비동기 처리 성공~

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -6,29 +6,29 @@ $('#selectAll').change(function() {
 	}
 });*/
 document.addEventListener('DOMContentLoaded', function() {
-    // 전체 선택 체크박스와 개별 체크박스 가져오기
-    const selectAllCheckbox = document.getElementById('selectAll');
-    const individualCheckboxes = document.querySelectorAll('.checkbox2');
+	// 전체 선택 체크박스와 개별 체크박스 가져오기
+	const selectAllCheckbox = document.getElementById('selectAll');
+	const individualCheckboxes = document.querySelectorAll('.checkbox2');
 
-    // 전체 선택 체크박스의 변경 이벤트를 처리
-    selectAllCheckbox.addEventListener('change', function() {
-        individualCheckboxes.forEach(function(checkbox) {
-            checkbox.checked = this.checked;
-        }, this);
-    });
+	// 전체 선택 체크박스의 변경 이벤트를 처리
+	selectAllCheckbox.addEventListener('change', function() {
+		individualCheckboxes.forEach(function(checkbox) {
+			checkbox.checked = this.checked;
+		}, this);
+	});
 
-    // 개별 체크박스의 변경 이벤트를 처리.
-    individualCheckboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            // 모든 개별 체크박스가 선택되었는지 확인
-            const allChecked = Array.from(individualCheckboxes).every(function(checkbox) {
-                return checkbox.checked;
-            });
+	// 개별 체크박스의 변경 이벤트를 처리.
+	individualCheckboxes.forEach(function(checkbox) {
+		checkbox.addEventListener('change', function() {
+			// 모든 개별 체크박스가 선택되었는지 확인
+			const allChecked = Array.from(individualCheckboxes).every(function(checkbox) {
+				return checkbox.checked;
+			});
 
-            // 모든 개별 체크박스가 선택되었다면 전체 선택 체크박스도 선택
-            selectAllCheckbox.checked = allChecked;
-        });
-    });
+			// 모든 개별 체크박스가 선택되었다면 전체 선택 체크박스도 선택
+			selectAllCheckbox.checked = allChecked;
+		});
+	});
 });
 
 $('.delete-all-btn').click(function() {
@@ -56,28 +56,30 @@ $('.delete-all-btn').click(function() {
 	}
 });
 
-// 총 가격을 업데이트하는 함수
 
 function changeQuantity(amount, element) {
-	
+ 
 	var cartItemQtyElement = element.parentElement.querySelector('#cartItemQty');
 	var currentQty = parseInt(cartItemQtyElement.value);
 	var newQty = currentQty + amount;
 	var relatedTextbox = element.parentElement.querySelector('.test');
 	var cartItemId = relatedTextbox.dataset.cartitemid;
+	var cartTotPriceElement = document.getElementById('priceTest');
+	var cartTot = cartTotPriceElement.value;
 	if (newQty >= 1) {
 		cartItemQtyElement.value = newQty;
-
+	
 		$.ajax({
 			url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/updateQty/' + cartItemId,
 			type: 'POST',
-			data: { cartItemId: cartItemId, cartItemQty: newQty},
+			data: { cartItemId: cartItemId, cartItemQty: newQty, cartTot:cartTot},
 			success: function(data) {
 				console.log('카트 아이템 수량이 업데이트되었습니다.');
-				var newPrice = data.cartTotPrice;
-				debugger;
-				document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
-				location.reload();
+				var newPrice = data.cartTot;
+				$('#price100 span#totalPrice').text(newPrice + '원');
+				//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
+				//location.reload();
+				calculateTotalPrice(cartId);
 			},
 			error: function() {
 				console.error('카트 아이템 수량 업데이트 중 오류가 발생했습니다.');
@@ -88,18 +90,20 @@ function changeQuantity(amount, element) {
 var cartId = 5;
 calculateTotalPrice(cartId);
 function calculateTotalPrice(cartId) {
-	
+
 	console.log(cartId);
 	$.ajax({
 		url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/' + cartId,
 		type: 'GET',
 		success: function(data) {
 			var newPrice = data.cartTotPrice;
-			document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
+			//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
 			console.log(document.getElementById('cartTotPrice'));
+			$('#cartTotPrice100 span#cartTotPrice').text('총 주문금액 : ' + newPrice + '원');
 		},
 		error: function(error) {
 			console.error('Error:', error);
 		}
 	});
 }
+
