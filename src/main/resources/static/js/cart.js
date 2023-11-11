bf9ffce2e59312d8e5eb4b5c4b0e7171f5b2f8ae
qlsjsek2 <qlsjsek2@naver.com>
2023-11-11 19:13:01 +0900

    카트 거의 끝났어요~~ 비동기 처리 모두 완료!! 수량 텍스트로 변경 추가!

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,176 +1,173 @@
-//체크박스 전체선택, 개별선택
-const allChk = document.querySelector('#checkboxAll');
-allChk.addEventListener('click', function() {
-	const isChecked = allChk.checked;
-	const checkbox = document.querySelectorAll('.checkbox');
+$(document).ready(function () {
+    // 페이지 로딩이 완료되면 실행될 코드
+    calculateTotalPrice();
 
-	if (isChecked) {
-		for (i = 0; i < checkbox.length; i++) {
-			checkbox[i].checked = true;
+	//체크박스 전체선택과,개별선택 코드
+    const allChk = document.querySelector('#checkboxAll');
+    const checkboxes = document.querySelectorAll('.checkbox');
 
-		}
-	} else {
-		for (i = 0; i < checkbox.length; i++) {
-			checkbox[i].checked = false;
-		}
-	}
+    allChk.addEventListener('click', function () {
+        const isChecked = allChk.checked;
+        checkboxes.forEach(function (checkbox) {
+            checkbox.checked = isChecked;
+        });
+        calculateTotalPrice();
+    });
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('click', function () {
+            const cnt = checkboxes.length;
+            const checkedCnt = document.querySelectorAll('.checkbox:checked').length;
+            allChk.checked = cnt === checkedCnt;
+            calculateTotalPrice();
+        });
+    });
+
+	//장바구니 상품 삭제 기능
+    $('.delete-all-btn').click(function () {
+        const checkedItems = document.querySelectorAll('.checkbox:checked');
+        const cartItemIds = Array.from(checkedItems).map(item => parseInt(item.dataset.cartItemId));
+
+        if (cartItemIds.length > 0) {
+            $.ajax({
+                url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/deleteByCartItems',
+                type: 'DELETE',
+                contentType: 'application/json',
+                data: JSON.stringify(cartItemIds),
+                success: function (response) {
+                    console.log('삭제 성공:', response);
+                    alert("삭제되었습니다.");
+                    location.reload();
+                    calculateTotalPrice();
+                },
+                error: function (error) {
+                    console.error('삭제 실패:', error);
+                }
+            });
+        } else {
+            alert('삭제할 상품을 선택해주세요.');
+        }
+    });
+
+    // 수량변경 이벤트 리스너 연결
+    $('.change-quantity-btn').click(function () {
+        const amount = 1; // 변경할 수량
+        const element = this; // 클릭된 버튼 엘리먼트
+        changeQuantity(amount, element);
+    });
 });
 
-const checkbox = document.querySelectorAll('.checkbox');
-for (const e of checkbox) {
-	e.addEventListener('click', e => {
-		const cnt = checkbox.length;
-		const checkedCnt = document.querySelectorAll('.checkbox:checked').length;
-		if (cnt == checkedCnt) {
-			document.querySelector('#checkboxAll').checked = true;
-		} else {
-			document.querySelector('#checkboxAll').checked = false;
-		}
-	})
+//view 맨 아래 총 주문금액 계산
+function calculateTotalPrice() {
+    // 모든 체크박스 엘리먼트 선택
+    const checkboxes = document.querySelectorAll('.checkbox');
+
+    let total = 0;
+
+    checkboxes.forEach(function (checkbox) {
+        const cartItemId = checkbox.dataset.cartItemId;
+
+        // 수량 정보 가져오기 (여기서는 예시로 1로 가정)
+        const productQty = 1;
+        // Ajax 호출을 통해 상품 가격을 서버에서 가져옴
+        $.ajax({
+            url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/calculate/' + cartItemId,
+            type: 'GET',
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                const productPrice = parseFloat(data);
+                //console.log(productPrice);
+                // 체크된 경우에만 가격을 더함
+                if (checkbox.checked) {
+                    total += productPrice * productQty;
+                }
+            },
+            error: function (error) {
+                console.error('Error:', error);
+            }
+        });
+    });
+
+    // 전체 주문금액 업데이트
+    $('#cartTotPrice100 span#cartTotPrice').text('총 주문금액 : ' + total + '원');
 }
 
-//전체삭제,선택삭제
-$('.delete-all-btn').click(function() {
-	var checkedItems = document.querySelectorAll('.checkbox:checked');
-	var cartItemIds = Array.from(checkedItems).map(item => parseInt(item.dataset.cartItemId));
-
-	if (cartItemIds.length > 0) {
-		$.ajax({
-			url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/deleteByCartItems',
-			type: 'DELETE',
-			contentType: 'application/json',
-			data: JSON.stringify(cartItemIds),
-			success: function(response) {
-				console.log('삭제 성공:', response);
-				alert("삭제되었습니다.");
-				location.reload();
-			},
-			error: function(error) {
-				console.error('삭제 실패:', error);
-			}
-		});
-	} else {
-		alert('삭제할 상품을 선택해주세요.');
-	}
-});
-
-//수량변경
+//상품 수량 변경, 수량변경으로 인한 함수 호출로 총 주문금액, 개별 상품 금액 업데이트
 function changeQuantity(amount, element) {
-	var cartItemQtyElement = element.parentElement.querySelector('#cartItemQty');
-	var currentQty = parseInt(cartItemQtyElement.value);
-	var newQty = currentQty + amount;
-	var relatedTextbox = element.parentElement.querySelector('.test');
-	var cartItemId = relatedTextbox.dataset.cartitemid;
-	
-	var cartTotPriceElement = document.getElementById('priceTest');
-	var cartTot = parseInt(cartTotPriceElement.value);
+    const cartItemQtyElement = element.parentElement.querySelector('#cartItemQty');
+    const currentQty = parseInt(cartItemQtyElement.value);
+    const newQty = currentQty + amount;
+    const relatedTextbox = element.parentElement.querySelector('.test');
+    const cartItemId = relatedTextbox.dataset.cartitemid;
 
-	if (newQty >= 1) {
-		cartItemQtyElement.value = newQty;
-	}
-	//수량변경 후 가격 업데이트
-	$.ajax({
-		url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/updateQty/' + cartItemId,
-		type: 'POST',
-		data: { cartItemId: cartItemId, cartItemQty: newQty},
-		success: function(data) {
-			console.log('카트 아이템 수량이 업데이트되었습니다.');
-			//var newPrice = data.cartTot;
-			//$('#price100 span#totalPrice').text(newPrice + '원');
-			//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
-			//location.reload();
-			calculateTotalPrice(cartId);
-			updateTotalPrice(cartItemId);
-		},
-		error: function() {
-			console.error('카트 아이템 수량 업데이트 중 오류가 발생했습니다.');
-		}
-	});
+    if (newQty >= 1) {
+        cartItemQtyElement.value = newQty;
+
+        // 서버에 수량 업데이트를 요청
+        $.ajax({
+            url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/updateQty/' + cartItemId,
+            type: 'POST',
+            data: { cartItemId: cartItemId, cartItemQty: newQty },
+            success: function (data) {
+                console.log('카트 아이템 수량이 업데이트되었습니다.');
+                // 개별 상품의 가격 업데이트
+                updateTotalPrice(cartItemId);
+                // 전체 주문금액 재계산
+                calculateTotalPrice();
+            },
+            error: function () {
+                console.error('카트 아이템 수량 업데이트 중 오류가 발생했습니다.');
+            }
+        });
+    }
+    
 }
 
-//수량별 가격 업데이트
-//var cartId=5
-//calculateTotalPrice(cartId)
-function calculateTotalPrice(cartId) {
-	var cartIdElement = document.getElementById('cartId');
-	var cartId = cartIdElement.value;
-	console.log(cartId);
-	$.ajax({
-		url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/' + cartId,
-		type: 'GET',
-		success: function(data) {
-			newPrice = data.cartTotPrice;
-			//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
-			console.log(document.getElementById('cartTotPrice'));
-			$('#cartTotPrice100 span#cartTotPrice').text('총 주문금액 : ' + newPrice + '원');
-		},
-		error: function(error) {
-			console.error('Error:', error);
-		}
-	});
+//개별 상품 수량변경으로 인한 금액 업데이트
+function updateTotalPrice(cartItemId) {
+    $.ajax({
+        url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/calculate/' + cartItemId,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $('#totalPrice[data-cartItemId="' + cartItemId + '"]').html(data + '원');
+        },
+        error: function (error) {
+            console.error('Error:', error);
+        }
+    });
 }
 
+//상품 수량 text로 수량변경, 함수호출로 총금액,개별금액 업데이트
+function updateQuantity(input) {
+    input.addEventListener('input', function () {
+        const newQty = parseInt(input.value);
+        if (!isNaN(newQty) && newQty >= 1) {
+            const relatedTextbox = input.parentElement.querySelector('.test');
+            const cartItemId = relatedTextbox.dataset.cartitemid;
 
-
-
-/*document.querySelectorAll('.checkbox2').forEach(function(checkbox) {
-	checkbox.addEventListener('change', function() {
-		if (!this.checked) {
-			var cartId = 5;
-			var cartTotPriceElement = document.getElementById('priceTest');
-			var cartTot = parseInt(cartTotPriceElement.value);
-			calculateTotalPrice(cartId);
-			function calculateTotalPrice(cartId) {
-				var checkedItems = document.querySelectorAll('.checkbox2:not(checked)');
-				var cartItemIds = Array.from(checkedItems).map(item => parseInt(item.dataset.cartItemId));
-				if (cartItemIds.length > 0) {
-					$.ajax({
-						url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/' + cartId,
-						type: 'GET',
-						success: function(data) {
-							var newPrice = data.cartTotPrice;
-							var tot = newPrice-cartTot;
-							//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
-							console.log(document.getElementById('cartTotPrice'));
-							$('#cartTotPrice100 span#cartTotPrice').text('총 주문금액 : ' + tot + '원');
-						},
-						error: function(error) {
-							console.error('Error:', error);
-						}
-					});
-				}
-
-			}
-		}else {
-			var cartId = 5;
-			var cartTotPriceElement = document.getElementById('priceTest');
-			var cartTot = parseInt(cartTotPriceElement.value);
-			calculateTotalPrice(cartId);
-			function calculateTotalPrice(cartId) {
-				var checkedItems = document.querySelectorAll('.checkbox2:not(checked)');
-				var cartItemIds = Array.from(checkedItems).map(item => parseInt(item.dataset.cartItemId));
-				if (cartItemIds.length > 0) {
-					$.ajax({
-						url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/' + cartId,
-						type: 'GET',
-						success: function(data) {
-							var newPrice = data.cartTotPrice;
-							var tot = newPrice+cartTot;
-							//document.getElementById('cartTotPrice').innerHTML = newPrice + '원';
-							console.log(document.getElementById('cartTotPrice'));
-							$('#cartTotPrice100 span#cartTotPrice').text('총 주문금액 : ' + tot + '원');
-						},
-						error: function(error) {
-							console.error('Error:', error);
-						}
-					});
-				}
-
-			}
-		}
-	});
-});
-*/
+            // 서버에 수량 업데이트를 요청
+            $.ajax({
+                url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/updateQty/' + cartItemId,
+                type: 'POST',
+                data: { cartItemId: cartItemId, cartItemQty: newQty },
+                success: function (data) {
+                    console.log('카트 아이템 수량이 업데이트되었습니다.');
+                    // 개별 상품의 가격 업데이트
+                    updateTotalPrice(cartItemId);
+                    // 전체 주문금액 재계산
+                    calculateTotalPrice();
+                },
+                error: function () {
+                    console.error('카트 아이템 수량 업데이트 중 오류가 발생했습니다.');
+                }
+            });
+        } else {
+            console.log('유효하지 않은 수량입니다.');
+        }
+    });
+}
 
 /*function goBuy(){
 	const checkedCnt = document.querySelectorAll('.checkbox:checked').length;
@@ -191,22 +188,7 @@ function calculateTotalPrice(cartId) {
 	}
 	
 }*/
-  function updateTotalPrice(cartItemId) {
-    console.log(cartItemId);
-    $.ajax({
-      url: '/2023-05-JAVA-DEVELOPER-final-project-team1-mango/cart_main/calculate/'+cartItemId,
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        $('#price100 span#totalPrice').text(data + '원');
-      	console.log(data);
-      },
-      
-      error: function(error) {
-        console.error('Error:', error);
-      }
-    });
-  }
+
 
 
 
