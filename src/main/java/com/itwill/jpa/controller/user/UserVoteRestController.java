54836801560962c477b7e7a04127efcf691ddb1f
유채린 <@MarianaYou>
2023-11-14 16:08:56 +0900

    ..

--- src/main/java/com/itwill/jpa/controller/user/UserVoteRestController.java
+++ src/main/java/com/itwill/jpa/controller/user/UserVoteRestController.java
@@ -1,8 +1,5 @@
 package com.itwill.jpa.controller.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itwill.jpa.dao.user.UserDaoImpl;
 import com.itwill.jpa.dto.user.UserVoteDto;
 import com.itwill.jpa.service.user.UserVotesericeImpl;
 
@@ -20,15 +18,22 @@ public class UserVoteRestController {
 
 	@Autowired
 	UserVotesericeImpl userVotesericeImpl;
+	@Autowired
+	UserDaoImpl userDaoImpl;
+	
 	@LoginCheck
 	@Operation(summary = "회원 투표 업데이트[완료]")
 	@PutMapping(value = "/userVoteUpdate/{userId}/{voteId}")
 	public ResponseEntity<?> updateUserVoteId(@PathVariable(name = "userId") String userId,
-	                                           @PathVariable(name = "voteId") Long voteId) {
+	                                           @PathVariable(name = "voteId") Long voteId) throws Exception {
 	    // userId와 voteId를 사용하여 업데이트 로직을 수행
 	    try {
-	        UserVoteDto updatedUserVoteId = userVotesericeImpl.updateUserVoteId(userId, voteId);
+	        if (userDaoImpl.findUser(userId).getVote()!=null) {
+	        	return new ResponseEntity<>("이미 투표가 완료된 회원입니다.", HttpStatus.CONFLICT);
+			}
+	    	UserVoteDto updatedUserVoteId = userVotesericeImpl.updateUserVoteId(userId, voteId);
 	        return new ResponseEntity<>(updatedUserVoteId, HttpStatus.OK);
+	        
 	    } catch (RuntimeException e) {
 	        return new ResponseEntity<>("없는 회원입니다.", HttpStatus.NOT_FOUND);
 	    } catch (Exception e) {
--- src/main/resources/static/js/vote.js
+++ src/main/resources/static/js/vote.js
@@ -7,14 +7,14 @@ function voteEnd() {
 }
 
 
+
 function voteButtonClicked(button) {
     // 사용자 ID와 투표 ID 추출
     var voteId = $(button).data("voteid");
     var userId = $(button).data("userid");
-	console.log(userId);
-	console.log(voteId);
-	
-	
+    console.log(userId);
+    console.log(voteId);
+
     // 회원 투표 업데이트 실행
     $.ajax({
         type: "PUT",
@@ -32,16 +32,39 @@ function voteButtonClicked(button) {
                     window.location.href = "/2023-05-JAVA-DEVELOPER-final-project-team1-mango/productVote";
                 },
                 error: function (xhr, textStatus, errorThrown) {
+                    // 여기에 오류 메시지에 따른 처리를 추가합니다.
+                    if (xhr.status === 409) {
+                        alert("이미 투표가 완료된 회원입니다.");
+                        // 페이지 이동을 막기 위해 아래 코드 추가
+                        return;
+                    }
                     alert("투표 업데이트 중 오류가 발생했습니다.");
+                    // 페이지 이동을 막기 위해 아래 코드 추가
+                    window.location.href = "/2023-05-JAVA-DEVELOPER-final-project-team1-mango/productVote";
                 }
             });
         },
         error: function (xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            if (xhr.status === 409) {
+                alert("이미 투표가 완료된 회원입니다.");
+                // 페이지 이동을 막기 위해 아래 코드 추가
+                return;
+            }
             alert("로그인이 필요한 서비스입니다.");
-             window.location.href = "/2023-05-JAVA-DEVELOPER-final-project-team1-mango/user_login_form";
+            window.location.href = "/2023-05-JAVA-DEVELOPER-final-project-team1-mango/user_login_form";
         }
     });
 }
+
+
+
+
+
+
+
+
+
 
 // endDate는 추후 VoteDate의 데이터로 변경예정
 var endDate = new Date('2023-11-15T23:59:59').getTime();
